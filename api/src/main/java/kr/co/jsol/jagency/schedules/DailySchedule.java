c3e651c0461203ccaed595930142b153dbf497d5
leeseongbok <lsb@j-sol.co.kr>
2024-10-31 14:24:54 +0900

    [Feat] 매일 버전 체크하는 기능 추가

---
+++ api/src/main/java/kr/co/jsol/jagency/schedules/DailySchedule.java
@@ -0,0 +1,26 @@
+package kr.co.jsol.jagency.schedules;
+
+import kr.co.jsol.jagency.application.VersionChecker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DailySchedule {
+    private final Logger log = LoggerFactory.getLogger(DailySchedule.class);
+    private final VersionChecker versionChecker;
+
+    public DailySchedule(VersionChecker versionChecker) {
+        this.versionChecker = versionChecker;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void dailySchedule() {
+        log.info("Daily Schedule");
+
+        // version check
+        log.info("[Daily Schedule - 001] check j-agency version");
+        versionChecker.init();
+    }
+}
